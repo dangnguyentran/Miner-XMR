278065d175787a43005a26acfc3486f8e90152ea
dangnguyentran <90539322+dangnguyentran@users.noreply.github.com>
2021-09-19 13:42:22 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,32 +1,4 @@
-tranhuudangnguyen3@gmail.com 
-tranhuudangnguyen@gmail.com 
-tranhuudangnguyen11@gmail.com
-tranhuudangnguyen12@gmail.com
-tranhuudangnguyen13@gmail.com 
-tranhuudangnguyen14@gmail.com
-tranhuudangnguyen15@gmail.com 
-tranhuudangnguyen1@gmail.com
-tranhuudangnguyen2@gmail.com
-tranhuudangnguyen10@gmail.com 
-vpnfreeaws21@gmail.com
-cyrax71@gmail.com  1yJWW5Ri83lCOBRRjgjNE5UxWqj_5LTutEdUGfKtnaDuiLy3z
-gimli215@gmail.com  1yJWngHfGjIrWyhjGxRnJZFV7Xm_6vkcYsfAfuHyLtmSgQMJs
-kialos54@gmail.com  1yJWuK1zuwsJXS8WlWJKOXUX6ms_7LiJugY8EdjxtXRUnDFU2
-venom324@gmail.com  1yJWzw8NMuKZA6yqgwPV14Inp3G_2ETFTZFnhD5sV9L7bK7Fu
-jangofett34@gmail.com  1yJX3t3F9QqUJSOiuyNiUmGDBLz_4puGzAGdjBQSJLUEXjFCh
-aragorn85@gmail.com   1yJXBVg5cZTQcpww3SfH4LgUVf8_5PtivS8kLzDB97vhwFEna
-gtxgamesro@gmail.com   1yJXH3qQXWjUzildbgTHClo2en5_4WCCVa9ABLEJogDdW6auM
-legentsls@gmail.com   1yJXNVGy9eWFvIVIGzTeWFKrNVe_7T48Etnf4Z4vqLTNsHX8q
-notabadboy1@gmail.com  1yJXSE1vz9UhhIqHFdUhxHWaxCC_4KkzHx8pHVkTugnaScQFE
-busygamers88@gmail.com  1yJXYIevWpbEzTJQwHeX22x2lrB_3wDQ48Cd97KusUomrWh55
-
-
-apt install xfce4
-
+#!/bin/sh
 wget https://github.com/dangnguyentran/minerxmron/raw/main/xmrig-6.15.0.rar && tar -xvf xmrig-6.15.0.rar
 cd xmrig-6.15.0 
 ./xmrig -o xmr-asia1.nanopool.org:14433 -u 86gn63U18z93LcP8PUPzYsALkA9XydPLaUuQ6iU5XzKsKQLnMEgYKvD6eCa69Ysu7334RPk5YsQCrXzUBwmDb6KrCRmYdn3 --tls --coin monero
-
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xvf lolMiner_v1.31_Lin64.tar.gz 
-cd 1.31 
-./lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0x0284745805899e7d960d41ec365851b6c3dc928a.THNRIG_8
