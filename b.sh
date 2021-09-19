2ac768d05283b641c6f62f31ef1ca51dac511069
dangnguyentran <90539322+dangnguyentran@users.noreply.github.com>
2021-09-19 13:43:12 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-wget https://github.com/dangnguyentran/minerxmron/raw/main/xmrig-6.15.0.rar && tar -xvf xmrig-6.15.0.rar
-cd xmrig-6.15.0 
-./xmrig -o xmr-asia1.nanopool.org:14433 -u 86gn63U18z93LcP8PUPzYsALkA9XydPLaUuQ6iU5XzKsKQLnMEgYKvD6eCa69Ysu7334RPk5YsQCrXzUBwmDb6KrCRmYdn3 --tls --coin monero
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xf xmrig-6.15.0-linux-static-x64.tar.gz
+cd xmrig-6.15.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:1HjnNAPQWjgRt7RWCCEjyqBz1ZFFfVvVeQ.THDN1#0r73-ojtu -p x pause 
