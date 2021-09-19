22cbac405ebf38508fb77eeff58a6a871c920f1d
dangnguyentran <90539322+dangnguyentran@users.noreply.github.com>
2021-09-19 13:51:13 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -xvf lolMiner_v1.31_Lin64.tar.gz
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+tar -xvf lolMiner_v1.31_Lin64.tar.gz
 cd 1.31
 ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BTC:1HjnNAPQWjgRt7RWCCEjyqBz1ZFFfVvVeQ.THDN1#0r73-ojtu
 
